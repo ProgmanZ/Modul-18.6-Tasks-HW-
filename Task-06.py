ab4bbd75bdb314903becfbed9fbd9008308fbe68
dumphill@gmail.com <Py956jch>
2021-12-28 18:26:16 +0300

    6 задача в разработке

--- Task-06.py
+++ Task-06.py
@@ -1,1 +1,21 @@
 # Задача 6. Сжатие
+
+# Введите строку: aaAAbbсaaaA
+# Закодированная строка: a2A2b2с1a3A1
+
+user_string = input('Введите строку: ')
+count = 1
+len_word = len(user_string)
+# result = user_string[0]
+result = user_string[0]
+ind = 0
+for i in range(0, len_word):
+    if user_string[i] == result[ind]:
+        count += 1
+    else:
+        result += str(count)
+        result += user_string[i-1]
+        count = 0
+        ind += 1
+
+print(result)
