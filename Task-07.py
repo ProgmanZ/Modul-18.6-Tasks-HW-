9180a1afed9dca5c2173e1a672868f1ad5918382
dumphill@gmail.com <Py956jch>
2022-01-17 18:34:36 +0300

    6 задача в разработке

--- Task-07.py
+++ Task-07.py
@@ -3,19 +3,23 @@
 
 flag = True
 
+
 def check_digit(list_digit):
     list_temp = [True if i.isdigit() else False for i in list_digit]
     return False in list_temp
 
+
 def search_nodigit(list_check):
     tmp_list = [i for i in list_check if not i.isdigit()]
-    return  tmp_list
+    return tmp_list
+
 
 while flag:
 
     ip_address = input('Введите IP: ').split('.')
     if len(ip_address) != 4:
         print('IP адрес - это четыре группы положительных чисел, разделённые точками')
+
 
     elif check_digit(ip_address):
         print(','.join(search_nodigit(ip_address)), ' - Проверьте введенные вами данные.\n'
